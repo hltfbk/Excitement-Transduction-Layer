c5489f29bed390fc9b3fc82d4336dd92d5b6749d
Kathrin Eichler <kathrin.eichler@dfki.de>
2013-04-29 13:29:33 +0200

    added comment (just a test)

--- tl/src/main/java/eu/excitementproject/tl/composition/exceptions/CategoryAnnotatorException.java
+++ tl/src/main/java/eu/excitementproject/tl/composition/exceptions/CategoryAnnotatorException.java
@@ -12,6 +12,9 @@ public class CategoryAnnotatorException extends Exception {
 	 */
 	private static final long serialVersionUID = -2290239170781685668L;
 
+	/** Exception thrown by CategoryAnnotatorException if category annotation fails.
+	 * @param message
+	 */
 	public CategoryAnnotatorException(String message) {
 		super(message);
 	}
