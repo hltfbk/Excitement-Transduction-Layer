40a66b63265a9ff3dfb8f71944b59b55c49efed1
Kathrin Eichler <kathrin.eichler@dfki.de>
2013-05-03 10:24:13 +0200

    added use case interfaces

---
+++ tl/src/main/java/eu/excitementproject/tl/composition/nodematcher/AbstractNodeMatcher.java
@@ -0,0 +1,12 @@
+package eu.excitementproject.tl.composition.nodematcher;
+
+import eu.excitementproject.tl.composition.api.NodeMatcher;
+
+/**
+An implementation of the {@link NodeMatcher} interface
+* @author Kathrin Eichler
+* 
+*/
+public abstract class AbstractNodeMatcher implements NodeMatcher {
+
+}
