74d62ad8f2e2bb5decbd4f670604cd91398e9f96
Lili <Lili@132.70.6.60>
2013-05-08 13:52:33 +0300

    re-structure a bit

---
+++ tl/src/main/java/eu/excitementproject/tl/structures/rawgraph/RandomEDA.java
@@ -0,0 +1,62 @@
+package eu.excitementproject.tl.structures.rawgraph;
+
+import java.util.Random;
+
+import org.apache.uima.jcas.JCas;
+
+import eu.excitementproject.eop.common.EDABasic;
+import eu.excitementproject.eop.common.EDAException;
+import eu.excitementproject.eop.common.configuration.CommonConfig;
+import eu.excitementproject.eop.common.exception.ComponentException;
+import eu.excitementproject.eop.common.exception.ConfigurationException;
+
+/**
+ * EDA, which generates random entailment decisions (RandomTEDecision)
+ * @author LiliKotlerman
+ *
+ */
+public class RandomEDA implements EDABasic<RandomTEDecision>{
+
+	private Random generator;
+	
+	public RandomEDA() {
+		try {
+			initialize(null);
+		} catch (ConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (EDAException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ComponentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void initialize(CommonConfig config) throws ConfigurationException,
+			EDAException, ComponentException {
+		
+		generator = new Random(); 
+		
+	}
+
+	public RandomTEDecision process(JCas aCas) throws EDAException,
+			ComponentException {
+		
+		return new RandomTEDecision(generator.nextDouble());
+	}
+
+	public void shutdown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void startTraining(CommonConfig c) throws ConfigurationException,
+			EDAException, ComponentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
---
+++ tl/src/main/java/eu/excitementproject/tl/structures/rawgraph/RandomTEDecision.java
@@ -0,0 +1,36 @@
+package eu.excitementproject.tl.structures.rawgraph;
+
+import eu.excitementproject.eop.common.DecisionLabel;
+import eu.excitementproject.eop.common.TEDecision;
+
+/**
+ * A TEDecision with a random confidence score and a corresponding DecisionLabel, based on a simple score heuristic
+ * @author LiliKotlerman
+ *
+ */
+public class RandomTEDecision implements TEDecision{
+	
+	final Double randomConfidence;
+	
+	public RandomTEDecision(Double randomConfidence){
+		this.randomConfidence = randomConfidence; 
+	}
+	
+	public DecisionLabel getDecision() {
+		if (randomConfidence > 0.95) return DecisionLabel.Paraphrase;
+		else if(randomConfidence > 0.8) return DecisionLabel.Entailment;
+		else if (randomConfidence < 0.5) return DecisionLabel.NonEntailment;
+		return DecisionLabel.Unknown;
+	}
+
+	public double getConfidence() {
+		return randomConfidence;
+	}
+
+	public String getPairID() {
+		// ToDo: understand how to extract this from the CAS (this method returns the entailment.Pair id as described in the CAS.)
+		// meanwhile return null
+		return null;
+	}
+	
+}
