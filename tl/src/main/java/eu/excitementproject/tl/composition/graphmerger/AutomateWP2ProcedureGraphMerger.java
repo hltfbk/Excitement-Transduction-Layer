6bb7823de0e4ebd1ea3b5aaac64bba4773a89136
LiliKotlerman <lili.dav@gmail.com>
2013-05-21 16:08:45 +0300

    Continue writing WP2GraphMerger

--- tl/src/main/java/eu/excitementproject/tl/composition/graphmerger/AutomateWP2ProcedureGraphMerger.java
+++ tl/src/main/java/eu/excitementproject/tl/composition/graphmerger/AutomateWP2ProcedureGraphMerger.java
@@ -1,12 +1,18 @@
 package eu.excitementproject.tl.composition.graphmerger;
 
+import java.util.HashSet;
 import java.util.Set;
 
+import eu.excitementproject.eop.common.DecisionLabel;
 import eu.excitementproject.eop.common.EDABasic;
+import eu.excitementproject.eop.common.TEDecision;
 import eu.excitementproject.eop.lap.LAPAccess;
 import eu.excitementproject.tl.composition.exceptions.GraphMergerException;
 import eu.excitementproject.tl.structures.fragmentgraph.FragmentGraph;
+import eu.excitementproject.tl.structures.fragmentgraph.FragmentGraphEdge;
 import eu.excitementproject.tl.structures.rawgraph.EntailmentGraphRaw;
+import eu.excitementproject.tl.structures.rawgraph.EntailmentRelation;
+import eu.excitementproject.tl.structures.rawgraph.EntailmentUnit;
 
 public class AutomateWP2ProcedureGraphMerger extends AbstractGraphMerger {
 
@@ -31,13 +37,82 @@ public class AutomateWP2ProcedureGraphMerger extends AbstractGraphMerger {
 	public EntailmentGraphRaw mergeGraphs(FragmentGraph fragmentGraph,
 			EntailmentGraphRaw workGraph) throws GraphMergerException {
 		
-		// The the work graph is empty or null - just copy the fragment graph nodes/edges, there's nothing else to merge
+		// If the work graph is empty or null - just copy the fragment graph nodes/edges, there's nothing else to merge
 		if(workGraph.isEmpty() || workGraph==null) workGraph = new EntailmentGraphRaw(fragmentGraph);
 		else{ 
-			// TODO Implement the WP2 flow			
+			// Implement the WP2 flow
+			
+			// 1. Add the nodes and edges from the fragment graph into the work graph
+			for (FragmentGraphEdge fragmentGraphEdge : fragmentGraph.edgeSet()){
+				workGraph.addEdgeFromFrahmentGraph(fragmentGraphEdge);
+			}
+				// find the work graph node
+			EntailmentUnit newBaseStatement = workGraph.getVertex(fragmentGraph.getBaseStatement().getText());
+			
+			// 2. For each base statement in the work graph perform merge with the fragment graph?
+			// TODO find out how exactly :) 
+			
+			for (EntailmentUnit workGraphBaseStatement : workGraph.getBaseStatements()){
+					// find the set of edges to add
+				Set<EntailmentRelation> edgesToAdd = mergeOneBaseStatement(workGraph, workGraphBaseStatement, newBaseStatement);
+					// add the edges to the work graph
+				for (EntailmentRelation edge : edgesToAdd){
+					workGraph.addEdge(edge.getSource(), edge.getTarget(), edge);
+				}
+			}
 		}
 		
 		return workGraph;		
 	}
+	
+	
+
+	/**
+	 * Performs one merge cycle - per one base statement from the workGraph
+	 * @param workGraph
+	 * @param workGraphbaseStatement
+	 * @param newBaseStatement
+	 * @return all the edges that should be added to merge the two graphs
+	 * @throws GraphMergerException
+	 */
+	private Set<EntailmentRelation> mergeOneBaseStatement(EntailmentGraphRaw workGraph, EntailmentUnit workGraphBaseStatement, EntailmentUnit newBaseStatement) throws GraphMergerException {
+		
+		Set<EntailmentRelation> edgesToAdd = new HashSet<EntailmentRelation>(); 
+		
+		//Check if there is entailment between the two base statements
+		// If there's no entailment between the base statements - done, nothing else to merge
+		boolean isEntailment = false;
+			// check one direction: workGraphBaseStatement -> newBaseStatement
+		EntailmentRelation r = new EntailmentRelation(workGraphBaseStatement, newBaseStatement, this.getEda());
+		if (r.getLabel().equals(DecisionLabel.Entailment)) {
+			isEntailment=true;
+			// add the edge to the output only if observed entailing, according to the WP2 algo
+			// we don't need to store all the knowledge we have for WP2 graph merger
+			edgesToAdd.add(r); 
+		}
+
+			// check the other direction: newBaseStatement -> workGraphBaseStatement
+		r = new EntailmentRelation(newBaseStatement, workGraphBaseStatement, this.getEda());
+		if (r.getLabel().equals(DecisionLabel.Entailment)) {
+			isEntailment=true;
+			edgesToAdd.add(r); 
+		}
+		
+		if (!isEntailment) return edgesToAdd; // no entailment - we are done (return the empty edge set)
+		
+		// If there is entailment between the two statements
+
+		// TODO: need to memorize the direction of the entailment and check in this direction for 1st-level nodes 
+		// then propagate automatically
+			
+		
+		
+		return edgesToAdd;
+		
+	}
+	
+
+	
+	
 
 }
