72a4280bd2ad02997ac98269273f1df5510549d9
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2013-07-31 15:35:39 +0200

    - updated LAPUsageExample for iterating Malt parser parsing result

--- tl/src/main/java/eu/excitementproject/tl/demo/LAPUsageExample.java
+++ tl/src/main/java/eu/excitementproject/tl/demo/LAPUsageExample.java
@@ -4,19 +4,12 @@ import org.apache.uima.jcas.JCas;
 
 import eu.excitementproject.eop.lap.LAPAccess;
 import eu.excitementproject.eop.lap.LAPException;
-import eu.excitementproject.eop.lap.PlatformCASProber;
 import eu.excitementproject.eop.lap.dkpro.MaltParserEN;
-import eu.excitementproject.eop.lap.dkpro.MaltParserDE;
 import eu.excitementproject.tl.laputils.CASUtils;
 
-import org.apache.uima.cas.text.AnnotationIndex;
 //import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.tcas.Annotation;
 import org.uimafit.util.JCasUtil;
-import java.util.Iterator;
-import java.util.List;
 
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.dependency.Dependency;
 
@@ -38,10 +31,11 @@ public class LAPUsageExample {
 		// Initializing a MaltParser 
 		// (TreeTagger POS tagging & Lemmatization, then MaltParser dependency parsing)
 		// See MaltParserEN class of EOP (LAP module), for more detail 
+		//  ( e.g. how to use models other than default one, etc) 
 		LAPAccess malt = null; 
 		try {
 			malt = new MaltParserEN();  // without argument, it initialize with the default model 
-//			malt = new MaltParserDE();  // without argument, it initialize with the default model 
+			//malt = new MaltParserDE();  // use this for German 			
 		}
 		catch (LAPException e)
 		{
@@ -55,70 +49,53 @@ public class LAPUsageExample {
 		try {
 			aJCas = CASUtils.createNewInputCas(); // get one via CASUtils 
 
-			aJCas.setDocumentText("Hello. This is a text."); 
+			aJCas.setDocumentText("WP6 successfully developed the needed methods for the industrial use cases."); 
 			aJCas.setDocumentLanguage("EN"); 
-//			aJCas.setDocumentText("Ich habe Hunger."); 
-//			aJCas.setDocumentLanguage("DE"); 
 			malt.addAnnotationOn(aJCas);  // actual annotation is being done. takes some time. 
 			// Once the call addAnnotationOn successes, the aJCas has "sentence", "token", "lemma", "POS" and "Dependency" annotations. 
-			CASUtils.dumpCAS(aJCas); 
 		}
 		catch (LAPException e)
 		{
 			System.out.println(e.getMessage());
 			System.exit(2); 			
 		}
-		
-		
+
 		// Here, we will try to see/access (mainly) the dependency annotations in aJCas  
 
 		// Okay. now JCas holds text, sentence annotation, POS annotation, lemma annotation, 
-		// Dependency annotation, etc. 		
-		// Let's iterate over the tokens, and each token, let's see what lemmas and 
-		// dependencies they have. 
-
-		AnnotationIndex<Annotation> depIndex = aJCas.getAnnotationIndex(Dependency.type);
-		Iterator<Annotation> depItr = depIndex.iterator(); 
-
-		while (depItr.hasNext())
-		{
-			Dependency d = (Dependency) depItr.next(); 
-			String t = d.getCoveredText(); 
-			int b = d.getBegin(); 
-			int e = d.getEnd(); 
-			System.out.println(b); 
-			System.out.println(e); 
-			System.out.println(t); 
-		}
-		System.exit(1); 
+		// Dependency annotations, etc. Let's iterate over the dependency edges, and 
+		// see what they have. 
 		
-		AnnotationIndex<Annotation> tokenIndex = aJCas.getAnnotationIndex(Token.type);
-		Iterator<Annotation> tokenItr = tokenIndex.iterator(); 
 
-		System.out.println("start-end token/lemma/POS\n\t [DependencyRelation] --> [GovernerToken]"); 
-		while (tokenItr.hasNext())
-		{
-			// we are getting l, 
-			Token t = (Token) tokenItr.next(); 
-			int begin = t.getBegin(); 
-			int end = t.getEnd(); 
-			Lemma l = t.getLemma();
-			String tokenStr = t.getCoveredText(); 
-			String lemmaStr = l.getValue(); 
-			String posStr = t.getPos().getPosValue(); 
+		for (Dependency dep : JCasUtil.select(aJCas, Dependency.class)) {
 			
-			// lets get Dependency type annotation, that covers this lemma 
-			List<Dependency> dl = JCasUtil.selectCovered(aJCas, Dependency.class , begin, end);
+			// One Dependency annotation holds the information for a dependency edge. 
+			// Basically, 3 things; 
+			// It holds "Governor (points to a Token)", "Dependent (also to a Token)", 
+			// and relationship between them (as a string)
+			Token dependent = dep.getDependent(); 
+			Token governor = dep.getGovernor();
+			String dTypeStr = dep.getDependencyType(); 
 			
-			if (dl.size() == 0)
-				continue; 
+			// lets print them with full token information (lemma, pos, loc) 
+			// info for the dependent ... 
+			int dBegin = dependent.getBegin(); 
+			int dEnd = dependent.getEnd(); 
+			String dTokenStr = dependent.getCoveredText(); 
+			String dLemmaStr = dependent.getLemma().getValue(); 
+			String dPosStr = dependent.getPos().getPosValue(); 
 			
-			Dependency d = dl.get(0); 
-			String dTypeStr = d.getDependencyType(); 		
-			String governerTokenStr = d.getGovernor().getCoveredText(); 
-			System.out.println(begin + "-" + end + " " + tokenStr + "/" + lemmaStr + "/" + posStr);
-			System.out.println("\t "+ dTypeStr + " --> " + governerTokenStr); 
-			
+			// info for the governor ...  
+			int gBegin = governor.getBegin(); 
+			int gEnd = governor.getEnd(); 
+			String gTokenStr = governor.getCoveredText(); 
+			String gLemmaStr = governor.getLemma().getValue(); 
+			String gPosStr = governor.getPos().getPosValue(); 
+
+			// and finally print the edge with full info 
+			System.out.println(dBegin + "-" + dEnd + " " + dTokenStr + "/" + dLemmaStr + "/" + dPosStr);
+			System.out.println("\t ---"+ dTypeStr + " --> "); 
+			System.out.println("\t " + gBegin + "-" + gEnd + " " + gTokenStr + "/" + gLemmaStr + "/" + gPosStr);
 		}
 	
 	}
