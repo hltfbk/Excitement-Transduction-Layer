829ecf039f4e5b7023531a1fd5e2031326d68899
Kathrin Eichler <kathrin.eichler@dfki.de>
2013-10-18 12:52:08 +0200

    Added OMQ configuration file

--- tl/src/main/java/eu/excitementproject/tl/structures/visualization/GraphViewer.java
+++ tl/src/main/java/eu/excitementproject/tl/structures/visualization/GraphViewer.java
@@ -4,32 +4,32 @@ package eu.excitementproject.tl.structures.visualization;
 import java.awt.Dimension;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.File;
 
 import javax.swing.JFrame;
-import javax.swing.JTabbedPane;
 
 import org.jgrapht.graph.AbstractGraph;
 
-import eu.excitementproject.tl.composition.exceptions.EntailmentGraphCollapsedException;
+import eu.excitementproject.tl.composition.api.CollapsedGraphGenerator;
+import eu.excitementproject.tl.composition.collapsedgraphgenerator.SimpleCollapseGraphGenerator;
 import eu.excitementproject.tl.structures.collapsedgraph.EntailmentGraphCollapsed;
-import eu.excitementproject.tl.structures.fragmentgraph.FragmentGraph;
 import eu.excitementproject.tl.structures.rawgraph.EntailmentGraphRaw;
 
-@SuppressWarnings("unused")
 public class GraphViewer {
     
 	private static final Dimension DEFAULT_SIZE = new Dimension( 1000, 700 );
 
 	public static void drawRandomGraph() {
 		
-    	JFrame frame = new JFrame("E X C I T M E N T  |  g r a p h v i e w");
+    	JFrame frame = new JFrame("E X C I T E M E N T  |  g r a p h v i e w");
     	
     	try {
 
- //    			FragmentGraph g = FragmentGraph.getSampleGraph();
-    			EntailmentGraphRaw g = EntailmentGraphRaw.getSampleOuput(false);
- //   			EntailmentGraphCollapsed g = new EntailmentGraphCollapsed(new File("./src/test/outputs/WP2_public_data_CAS_XMI/nice_email_1/collapsed_graph.xml"));
+    			//FragmentGraph g = FragmentGraph.getSampleGraph();
+    			EntailmentGraphRaw gr = EntailmentGraphRaw.getSampleOuput(false);
+				CollapsedGraphGenerator collapser = new SimpleCollapseGraphGenerator();
+				EntailmentGraphCollapsed g = collapser.generateCollapsedGraph(gr, 0.2);
+
+    			//EntailmentGraphCollapsed g = new EntailmentGraphCollapsed(new File("./src/test/outputs/WP2_public_data_CAS_XMI/nice_email_1/collapsed_graph.xml"));
     		
     			System.out.println("GRAPH:\n\n" + g.toString() + "\n\n");
     			final GraphRenderer graphRenderer = new GraphRenderer(g);
@@ -112,15 +112,16 @@ public class GraphViewer {
 	public static void main(String[] argv) {
 
 		// something just to try, without parameters
-//		GraphViewer.drawRandomGraph();
+		GraphViewer.drawRandomGraph();
 		
 		// visualize a specific graph (could be either FragmentGraph, EntailmentGraphRaw, or EntailmentGraphCollapsed
+		/*
 		try{
-			EntailmentGraphCollapsed g = new EntailmentGraphCollapsed(new File("./src/test/outputs/WP2_public_data_CAS_XMI/nice_email_1/collapsed_graph.xml"));
+			EntailmentGraphCollapsed g = new EntailmentGraphCollapsed(new File("src/test/outputs/WP2_public_data_CAS_XMI/nice_email_1/collapsed_graph.xml"));
 			GraphViewer.drawGraph(g);
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
+		}*/
 		
 	}
 }
