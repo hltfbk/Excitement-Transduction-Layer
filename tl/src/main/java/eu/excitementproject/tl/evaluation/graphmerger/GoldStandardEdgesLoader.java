1a648464544bb6036b7c39a442d7c48656589d7a
LiliKotlerman <lili.dav@gmail.com>
2013-10-29 12:18:08 +0200

    Proceeding with graph evaluation procedure

---
+++ tl/src/main/java/eu/excitementproject/tl/evaluation/graphmerger/GoldStandardEdgesLoader.java
@@ -0,0 +1,118 @@
+package eu.excitementproject.tl.evaluation.graphmerger;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
+import eu.excitementproject.eop.common.DecisionLabel;
+import eu.excitementproject.tl.evaluation.exceptions.GraphEvaluatorException;
+import eu.excitementproject.tl.structures.rawgraph.EntailmentRelation;
+import eu.excitementproject.tl.structures.rawgraph.EntailmentUnit;
+import eu.excitementproject.tl.structures.rawgraph.utils.EdgeType;
+import eu.excitementproject.tl.structures.rawgraph.utils.TEDecisionWithConfidence;
+
+/**
+ * This is the class responsible for loading gold standard edges from manually annotated data
+ * @author Lili Kotlerman
+ *
+ */
+public class GoldStandardEdgesLoader {
+	
+	Set<EntailmentRelation> edges;
+	Map<String,String> nodeTextById;	
+	
+	public GoldStandardEdgesLoader() {
+		edges = new HashSet<EntailmentRelation>();
+		nodeTextById = new HashMap<String,String>(); //[id] [text]	
+	}
+	
+
+	/**
+	 * @return the edges
+	 */
+	public Set<EntailmentRelation> getEdges() {
+		return edges;
+	}
+
+
+
+	public void addAnnotations(String xmlAnnotationFilename) throws GraphEvaluatorException{		
+		// read all the nodes from xml annotation file and add them to the index 
+	   		try {
+					DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+					DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+					Document doc = dBuilder.parse(new File(xmlAnnotationFilename));
+   
+					doc.getDocumentElement().normalize();	     
+					doc.getDocumentElement().getNodeName();
+					NodeList nodes = doc.getElementsByTagName("node");
+					
+					// add nodes to the dictionary nodeTextById
+					for (int temp = 0; temp < nodes.getLength(); temp++) {    
+						Node xmlNode = nodes.item(temp);     
+						//node.getNodeName();   
+
+						Element nodeElement = (Element) xmlNode;
+					//	String text = nodeElement.getAttribute("original_text");
+						String id = nodeElement.getAttribute("id");
+						NodeList xmlChildNodes = xmlNode.getChildNodes();
+				       	for (int i = 0; i < xmlChildNodes.getLength(); i++) {    
+				       		Node child = xmlChildNodes.item(i);
+				       		if (child.getNodeName().equals("original_text")){
+							   	String text = child.getTextContent();
+				       			nodeTextById.put(id, text);
+							   	System.out.println("Added node with id <<"+id+">> and text <<"+text+">>");
+				       		}
+				       	}
+					}   										
+					
+					// load all the edges
+					NodeList entailmentRelationList = doc.getElementsByTagName("edge");
+					for (int temp = 0; temp < entailmentRelationList.getLength(); temp++) {    
+						Node er = entailmentRelationList.item(temp);     
+						er.getNodeName();     
+						Element erElement = (Element) er;
+						String src = erElement.getAttribute("source");
+						if (!nodeTextById.containsKey(src)) throw new GraphEvaluatorException("Annotation file "+xmlAnnotationFilename+" contains and edge with source node "+ src+ ", which is not presented in the nodes list");
+						String tgt = erElement.getAttribute("target");
+						if (!nodeTextById.containsKey(tgt)) throw new GraphEvaluatorException("Annotation file "+xmlAnnotationFilename+" contains and edge with target node "+ tgt+ ", which is not presented in the nodes list");
+						EntailmentUnit sourceUnit = new EntailmentUnit(nodeTextById.get(src), -1, "", "unknown"); // "-1" level means "unknown", put "" as complete statement text, since only the text of the node is compared when comparing edges
+						EntailmentUnit targetUnit = new EntailmentUnit(nodeTextById.get(tgt), -1, "", "unknown"); 
+						edges.add(new EntailmentRelation(sourceUnit, targetUnit, new TEDecisionWithConfidence(1.0, DecisionLabel.Entailment), EdgeType.MANUAL_ANNOTATION));
+					}
+				} catch (ParserConfigurationException | SAXException | IOException e) {
+					throw new GraphEvaluatorException("Problem loading annotations from file "+ xmlAnnotationFilename+ ".\n" + e.getMessage());
+				}		
+	}
+	
+	public static void main(String[] args) {
+		GoldStandardEdgesLoader loader = new GoldStandardEdgesLoader();
+		String annotationFilename = "./src/test/resources/WP2_gold_standard_annotation/_annotationExample.xml";
+		try {
+			loader.addAnnotations(annotationFilename);
+			int i=1;		
+			for (EntailmentRelation edge : loader.edges){
+				System.out.println(i+": "+edge);
+				i++;
+			}
+		} catch (GraphEvaluatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+	}
+}
