2068244b5c7b2fb066f7b130958aec84442deaef
LiliKotlerman <lili.dav@gmail.com>
2013-11-10 12:49:21 +0200

    Continue working with evaluation

---
+++ tl/src/test/java/eu/excitementproject/tl/evaluation/graphmerger/EvaluatorGraphMergerTest.java
@@ -0,0 +1,30 @@
+package eu.excitementproject.tl.evaluation.graphmerger;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import eu.excitementproject.tl.evaluation.exceptions.GraphEvaluatorException;
+
+public class EvaluatorGraphMergerTest {
+
+	@Test
+	public void test() {
+		GoldStandardEdgesLoader loader = new GoldStandardEdgesLoader();
+		String annotationFilename = "./src/test/resources/WP2_gold_standard_annotation/_annotationExample.xml";
+		try {
+			loader.addAnnotations(annotationFilename);
+			System.out.println("Loaded "+loader.getEdges().size()+" gold standard edges.");
+/*			int i=1;		
+			for (EntailmentRelation edge : loader.getEdges){
+				System.out.println(i+": "+edge);
+				i++;
+			}
+*/			System.out.println(EvaluatorGraphMerger.evaluate(loader.getEdges(), loader.getEdges()));
+		} catch (GraphEvaluatorException e) {
+			fail(e.getMessage());
+			e.printStackTrace();
+		}		
+	}
+
+}
