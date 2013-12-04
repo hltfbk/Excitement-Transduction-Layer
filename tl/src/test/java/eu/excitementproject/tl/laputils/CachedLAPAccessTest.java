9bcd5e6b4f3046fb5bec3fda0c8b35f3603c01bc
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2013-12-04 14:32:07 +0100

    added first version of cached LAP

---
+++ tl/src/test/java/eu/excitementproject/tl/laputils/CachedLAPAccessTest.java
@@ -0,0 +1,80 @@
+package eu.excitementproject.tl.laputils;
+
+import static org.junit.Assert.*;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+
+import eu.excitementproject.eop.lap.LAPAccess;
+import eu.excitementproject.eop.lap.PlatformCASProber;
+
+public class CachedLAPAccessTest {
+
+	@Test
+	public void test() {
+
+		// Set Log4J 
+		BasicConfigurator.resetConfiguration(); 
+		BasicConfigurator.configure(); 
+		Logger.getRootLogger().setLevel(Level.INFO);  // for UIMA (hiding < INFO) 
+		
+		// prepare one underlying LAP, say, TreeTagger 
+		LAPAccess underlyingLAP = null; 
+		CachedLAPAccess cachedLAP = null; 
+		
+		try {
+			underlyingLAP = new LemmaLevelLapEN(); 
+			cachedLAP = new CachedLAPAccess(underlyingLAP); 
+		}
+		catch(Exception e)
+		{
+			fail(e.getMessage()); 
+		}
+		
+		// now we have original LAP "underlyingLAP" and 
+		// cached (wrappered) one 
+		JCas originalCAS = null; 
+		JCas cachedCAS = null; 
+		String text = "This is a pipe."; 
+		String hypo = "Holy, this is not a pipe!"; 
+		try {
+			originalCAS = underlyingLAP.generateSingleTHPairCAS(text, hypo); 
+			cachedCAS = cachedLAP.generateSingleTHPairCAS(text, hypo); 
+		}
+		catch(Exception e)
+		{
+			fail(e.getMessage()); 
+		}
+		// Prover
+		try {
+			//PlatformCASProber.probeCas(originalCAS, System.out);
+			//PlatformCASProber.probeCas(cachedCAS, System.out);
+			// even dump? 
+			//PlatformCASProber.probeCasAndPrintContent(originalCAS, System.out); 
+			//PlatformCASProber.probeCasAndPrintContent(cachedCAS, System.out); 			
+		}
+		catch(Exception e)
+		{
+			fail(e.getMessage()); 
+		}	
+		
+		// caching test 
+		try {
+			cachedCAS = cachedLAP.generateSingleTHPairCAS(text, hypo);
+			cachedCAS = cachedLAP.generateSingleTHPairCAS(hypo, text);
+			cachedCAS = cachedLAP.generateSingleTHPairCAS(hypo, text);
+			originalCAS = underlyingLAP.generateSingleTHPairCAS(hypo, text); 
+			PlatformCASProber.probeCasAndPrintContent(originalCAS, System.out); 
+			PlatformCASProber.probeCasAndPrintContent(cachedCAS, System.out); 			
+		}
+		catch (Exception e)
+		{
+			fail(e.getMessage()); 
+		}	
+		
+	}
+
+}
