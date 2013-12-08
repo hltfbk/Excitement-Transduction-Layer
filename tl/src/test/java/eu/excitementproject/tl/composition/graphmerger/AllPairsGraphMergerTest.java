67161cddaadd83d5f0a9c480f76178213ec63dfa
LiliKotlerman <lili.dav@gmail.com>
2013-12-08 14:06:30 +0200

    Added a test for llPairsGraphMerger

---
+++ tl/src/test/java/eu/excitementproject/tl/composition/graphmerger/AllPairsGraphMergerTest.java
@@ -0,0 +1,47 @@
+package eu.excitementproject.tl.composition.graphmerger;
+
+import java.util.Set;
+import org.junit.Test;
+
+import eu.excitementproject.eop.common.EDABasic;
+import eu.excitementproject.eop.lap.LAPAccess;
+import eu.excitementproject.eop.lap.LAPException;
+import eu.excitementproject.eop.lap.dkpro.TreeTaggerEN;
+import eu.excitementproject.tl.composition.api.GraphMerger;
+import eu.excitementproject.tl.composition.exceptions.GraphMergerException;
+import eu.excitementproject.tl.structures.fragmentgraph.FragmentGraph;
+import eu.excitementproject.tl.structures.rawgraph.EntailmentGraphRaw;
+import eu.excitementproject.tl.structures.rawgraph.utils.RandomEDA;
+
+public class AllPairsGraphMergerTest {
+
+	@Test
+	public void test() {
+		
+		try {
+			LAPAccess lap = new TreeTaggerEN();
+			EDABasic<?> eda = new RandomEDA();
+			
+			GraphMerger merger = new AllPairsGraphMerger(lap,eda); 
+			
+			Set<FragmentGraph> fragmentGraphs = FragmentGraph.getSampleOutput();
+			System.out.println("**** Merging the following fragment graphs with  RandomEDA (complete statements printed) ****");			
+			for (FragmentGraph fg : fragmentGraphs) {
+				System.out.println(fg.getCompleteStatement().getText());
+			}
+			
+			System.out.println("Merged raw graph:");			
+			EntailmentGraphRaw rawGraph = merger.mergeGraphs(fragmentGraphs);
+			System.out.println(rawGraph.isEmpty());
+			System.out.println(rawGraph.toString());
+		} catch (LAPException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (GraphMergerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
