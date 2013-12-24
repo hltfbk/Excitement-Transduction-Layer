26aa607699744630b6d39f79f15900edfac3ba88
LiliKotlerman <lili.dav@gmail.com>
2013-12-24 15:43:10 +0200

    Updated Experiment to deal with full TIE

--- tl/src/main/java/eu/excitementproject/tl/experiments/NICE/Experiment.java
+++ tl/src/main/java/eu/excitementproject/tl/experiments/NICE/Experiment.java
@@ -1,6 +1,7 @@
 package eu.excitementproject.tl.experiments.NICE;
 
 import eu.excitementproject.eop.core.MaxEntClassificationEDA;
+import eu.excitementproject.eop.lap.dkpro.MaltParserEN;
 import eu.excitementproject.eop.lap.dkpro.TreeTaggerEN;
 import eu.excitementproject.tl.evaluation.utils.EvaluationMeasures;
 import eu.excitementproject.tl.structures.rawgraph.EntailmentGraphRaw;
@@ -20,25 +21,33 @@ public class Experiment extends AbstractExperiment {
 	 */
 	public static void main(String[] args) {
 
-		Experiment e = new Experiment(
+/*		Experiment e = new Experiment(
 //				"./src/test/resources/EOP_configurations/MaxEntClassificationEDA_Base_EN.xml",
-//				"./src/test/resources/EOP_configurations/MaxEntClassificationEDA_Base+WN+VO_EN.xml",
-				"./src/test/resources/EOP_configurations/MaxEntClassificationEDA_Base+WN+VO+TP+TPPos+TS_EN.xml",
+				"./src/test/resources/EOP_configurations/MaxEntClassificationEDA_Base+WN+VO_EN.xml",
 
 				"./src/test/resources/WP2_public_data_CAS_XMI/NICE_open", 19,
 				"/home/nastase/Projects/eop/excitement-transduction-layer/Excitement-Transduction-Layer/tl/src/test/outputs/WP2_public_data_CAS XMI/nice_email 1",
 				TreeTaggerEN.class,
 				MaxEntClassificationEDA.class
 				);
+*/		
 		
-		
+		Experiment e = new Experiment(
+				"./src/test/resources/EOP_configurations/MaxEntClassificationEDA_Base+WN+VO+TP+TPPos+TS_EN.xml",
+
+				"./src/test/resources/WP2_public_data_CAS_XMI/NICE_open", 19,
+				"/home/nastase/Projects/eop/excitement-transduction-layer/Excitement-Transduction-Layer/tl/src/test/outputs/WP2_public_data_CAS XMI/nice_email 1",
+				MaltParserEN.class,
+				MaxEntClassificationEDA.class
+				);
+
 		String gsAnnotationsDir = "./src/test/resources/WP2_gold_standard_annotation/NICE_open";
 		boolean includeFragmentGraphEdges = false;
 		EntailmentGraphRaw gr = e.buildRawGraph();
 		
 		//TODO Verify why FG edges are not found in the graph. Is it only for closure edges? 
 	//	System.out.println(gr);
-		for (double confidenceThreshold=0.5; confidenceThreshold<1; confidenceThreshold+=0.05){
+		for (double confidenceThreshold=0.0; confidenceThreshold<1; confidenceThreshold+=0.05){
 			EvaluationMeasures res = e.evaluateRawGraph(confidenceThreshold, gr, gsAnnotationsDir, includeFragmentGraphEdges);
 			System.out.println(confidenceThreshold+"\t"+res.getRecall()+"\t"+res.getPrecision()+"\t"+res.getF1());
 		}
