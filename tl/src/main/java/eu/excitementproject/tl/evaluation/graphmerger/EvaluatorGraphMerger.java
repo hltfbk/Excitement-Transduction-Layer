fc45d7c7f559dc094d8786aac31fe6b5cc6a6d04
LiliKotlerman <lili.dav@gmail.com>
2014-01-02 12:09:03 +0200

    Changed the implementation of the graph evaluation methodology not to assume GS and evaluated graphs have the same set of fragment graph edges.

--- tl/src/main/java/eu/excitementproject/tl/evaluation/graphmerger/EvaluatorGraphMerger.java
+++ tl/src/main/java/eu/excitementproject/tl/evaluation/graphmerger/EvaluatorGraphMerger.java
@@ -3,10 +3,14 @@ package eu.excitementproject.tl.evaluation.graphmerger;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
+
+import edu.mit.jwi.data.IClosable;
 import eu.excitementproject.tl.evaluation.exceptions.GraphEvaluatorException;
 import eu.excitementproject.tl.evaluation.utils.EvaluationMeasures;
 import eu.excitementproject.tl.structures.rawgraph.EntailmentRelation;
 import eu.excitementproject.tl.structures.rawgraph.utils.EdgeType;
+import gate.util.Out;
 
 /**
  * This class contains methods for evaluating graph merger results.
@@ -25,16 +29,18 @@ public class EvaluatorGraphMerger {
 	 * @return evaluation measures (recall, precision, f1).
 	 */
 	public static EvaluationMeasures evaluate(Set<EntailmentRelation> goldStandardEdges, Set<EntailmentRelation> evaluatedGraphEdges, boolean includeFragmentGraphEdges){
-		
+		Logger logger = Logger.getLogger("eu.excitementproject.tl.evaluation.graphmerger.EvaluatorGraphMerger");
+		logger.info("Evaluating graph edges. Include fragment graph = "+includeFragmentGraphEdges);
 		//TODO Transitive closure: GS fragment graphs contain closure edges. Understand whether GS merge phase has closure edges as well. If yes - need to evaluate our graphs with transitive closure. If not - need to add closure edges when generating fragment graphs.
 		// e.g. GS edge "Cannot retrieve tickets at Moonport station with card --> Cannot retrieve tickets" is not found in the raw graph
 		
 		// TODO consider adding a verification of whether GS and work graph have the same set of nodes
+		EvaluationMeasures eval = new EvaluationMeasures();
 		
-		double correctlyAddedEdges = 0.0;
+/*		double correctlyAddedEdges = 0.0;
 		Set<String> excludedEdges = new HashSet<String>();
 		for (EntailmentRelation gsEdge : goldStandardEdges){	
-//			boolean correct = false;
+			boolean correct = false;
 			for (EntailmentRelation workEdge : evaluatedGraphEdges){
 				if (!includeFragmentGraphEdges){
 					if (workEdge.getEdgeType().equals(EdgeType.FRAGMENT_GRAPH)) {
@@ -43,20 +49,79 @@ public class EvaluatorGraphMerger {
 					}
 				}
 				if (gsEdge.isSameSourceAndTarget(workEdge)) {
-//					System.out.println("+\t"+gsEdge);
+				//	logger.info("\n(+)\t"+gsEdge+"\t"+workEdge);
 					correctlyAddedEdges++;
-//					correct = true;
+					correct = true;
 					continue; // if found this gs edge - go look for the next one
 				}				
 			}
-//			if (!correct) System.out.println("-\t"+gsEdge);
+			if (!correct) logger.info("\n(-)\t"+gsEdge);
 		}
 		
-		EvaluationMeasures eval = new EvaluationMeasures();
-		eval.setPrecision(correctlyAddedEdges/(evaluatedGraphEdges.size()-excludedEdges.size()));
-		eval.setRecall(correctlyAddedEdges/(goldStandardEdges.size()-excludedEdges.size())); // assume that all fragment graph edges are present also in the gold standard
 		
+		System.out.println(correctlyAddedEdges+"\t"+evaluatedGraphEdges.size()+"\t"+excludedEdges.size());
+		eval.setPrecision(correctlyAddedEdges/(evaluatedGraphEdges.size()-excludedEdges.size()));
+		System.out.println(correctlyAddedEdges+"\t"+goldStandardEdges.size()+"\t"+excludedEdges.size());
+		eval.setRecall(correctlyAddedEdges/(goldStandardEdges.size()-excludedEdges.size())); // assume that all fragment graph edges are present also in the gold standard
+*/		
+
+		Set<String> gs = new HashSet<String>(); // will hold gs edges
+		Set<String> ee = new HashSet<String>(); // will hold evaluated edges
+		Set<String> correct = new HashSet<String>(); // will hold common edges (correctly added)
+		Set<String> fg = new HashSet<String>(); // will hold fragment graph edges to exclude, if need to exclude
+		
+		for (EntailmentRelation edge : goldStandardEdges){
+			gs.add(getSourceAndTargetString(edge));
+			correct.add(getSourceAndTargetString(edge));
+		}
+		logger.info("GS size: "+gs.size()+" distinct, "+goldStandardEdges.size()+" overall.");
+		
+		for (EntailmentRelation edge : evaluatedGraphEdges){
+			if (!includeFragmentGraphEdges){
+				if (edge.getEdgeType().equals(EdgeType.FRAGMENT_GRAPH)) {
+					fg.add(getSourceAndTargetString(edge));
+					continue; //add this edge to fragment graph edges list (fg), don't add to evaluated graph edges (ee)
+				}
+			}	
+			ee.add(getSourceAndTargetString(edge));
+		}
+		logger.info("Evaluated edges: "+evaluatedGraphEdges.size()+" overall (including fragment graph edges)");
+		if (!includeFragmentGraphEdges) logger.info("Evaluated edges: "+ee.size()+ " distinct, excluding "+fg.size()+" fragment graph edges");
+		else logger.info("Evaluated edges: "+ee.size()+" distinct");
+		
+		// remove fg edges from the gold standard
+		if (!includeFragmentGraphEdges) {
+			gs.removeAll(fg);
+			correct.removeAll(fg);			
+			logger.info("GS size without fragment graph edges: " + gs.size());
+		}
+
+		correct.retainAll(ee); // retain only common edges
+		
+		logger.info(correct.size()+" correctly assigned edges out of "+ee.size() +" assigned edges");
+		eval.setPrecision((0.0+correct.size())/ee.size());
+		logger.info(correct.size()+" correctly found edges out of "+gs.size()+" edges in GS");
+		eval.setRecall((0.0+correct.size())/gs.size()); 		
+		
+		for (EntailmentRelation edge : evaluatedGraphEdges){
+			if (correct.contains(getSourceAndTargetString(edge))) logger.debug("(+) "+ edge);
+			else if(!fg.contains(getSourceAndTargetString(edge)))  {
+				logger.debug("(- FP) " + edge);
+			}
+		}
+		for (EntailmentRelation edge : goldStandardEdges){
+			if (!correct.contains(getSourceAndTargetString(edge))){
+				if(!fg.contains(getSourceAndTargetString(edge))) {
+					logger.debug("(- FN) " + edge);
+				}
+			}
+		}
+				
 		return eval;
+	}
+	
+	private static String getSourceAndTargetString(EntailmentRelation edge){
+		return edge.getSource().getTextWithoutDoubleSpaces()+" -> "+edge.getTarget().getTextWithoutDoubleSpaces();
 	}
 	
 	public static EvaluationMeasures evaluate(Set<EntailmentRelation> goldStandardEdges, String goldStandardAnnotationsDirectory, boolean includeFragmentGraphEdges) throws GraphEvaluatorException {
