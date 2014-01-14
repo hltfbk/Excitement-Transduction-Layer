3fad8ad0ab09b8cd22f49e7c93a7d2a1ee5c6997
LiliKotlerman <lili.dav@gmail.com>
2014-01-14 14:22:53 +0200

    Changed NICE demo to use all the data

--- tl/src/main/java/eu/excitementproject/tl/demo/DemoUseCase1NICEEnglish.java
+++ tl/src/main/java/eu/excitementproject/tl/demo/DemoUseCase1NICEEnglish.java
@@ -20,7 +20,7 @@ public class DemoUseCase1NICEEnglish extends UseCaseOneDemo{
 	public DemoUseCase1NICEEnglish() {
 		
 		super("./src/test/resources/EOP_configurations/MaxEntClassificationEDA_Base_EN.xml",
-				"./src/test/resources/WP2_public_data_CAS_XMI/fragment_test", 33,
+				"./src/test/resources/WP2_public_data_CAS_XMI/NICE_open", 10,
 //				"./src/test/outputs/WP2_public_data_CAS_XMI/nice_email_1",
 				"./src/test/outputs/WP2_public_data_CAS_XMI/fragment_test",
 				TreeTaggerEN.class,
