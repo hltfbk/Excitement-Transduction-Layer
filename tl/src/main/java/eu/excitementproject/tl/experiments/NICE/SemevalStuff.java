260c57c3c54abea0c8e57bf5bc5ab974944d1d4d
LiliKotlerman <lili.dav@gmail.com>
2014-03-25 17:52:02 +0200

    first draft of tl-to-wp2 translator

--- tl/src/main/java/eu/excitementproject/tl/experiments/NICE/SemevalStuff.java
+++ tl/src/main/java/eu/excitementproject/tl/experiments/NICE/SemevalStuff.java
@@ -1,111 +1,129 @@
 package eu.excitementproject.tl.experiments.NICE;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
-import javax.xml.transform.TransformerException;
-
-import eu.excitementproject.tl.composition.exceptions.EntailmentGraphCollapsedException;
-import eu.excitementproject.tl.composition.exceptions.EntailmentGraphRawException;
-import eu.excitementproject.tl.composition.exceptions.GraphOptimizerException;
 import eu.excitementproject.tl.evaluation.exceptions.GraphEvaluatorException;
 import eu.excitementproject.tl.evaluation.graphmerger.GoldStandardEdgesLoader;
-import eu.excitementproject.tl.structures.collapsedgraph.EntailmentGraphCollapsed;
 import eu.excitementproject.tl.structures.rawgraph.EntailmentGraphRaw;
+import eu.excitementproject.tl.structures.rawgraph.EntailmentRelation;
+import eu.excitementproject.tl.structures.rawgraph.utils.EdgeType;
 
 public class SemevalStuff {
 
+	private static String getWP2edge(EntailmentRelation edge, Map<String,Set<String>> textToIdsMap){
+/*		Output example
+		<edge target="413572.txt_1_0" source="112459.txt_1_2" id="413572.txt_1_0-112459.txt_1_2">
+		<entailment_mod_insensitive> </entailment_mod_insensitive>
+		<entailment type="direct">yes</entailment>
+		</edge>
+*/		
+		String s="";
+		String targetText = edge.getTarget().getText();
+		String sourceText = edge.getSource().getText();
+		for (String tgtId : textToIdsMap.get(targetText)){
+			for (String srcId: textToIdsMap.get(sourceText)){
+				s += "\t<edge target=\""+tgtId+"\" source=\""+srcId+"\" id=\""+tgtId+"-"+srcId+"\">\n";
+				s += "\t\t<entailment_mod_insensitive> </entailment_mod_insensitive>\n";
+				if (edge.getEdgeType().is(EdgeType.TRANSITIVE_CLOSURE)) s+= "\t\t<entailment type=\"clousure\">yes</entailment>\n";
+				else s+= "\t\t<entailment type=\"direct\">yes</entailment>\n";
+				s+="\t</edge>\n";
+			}
+		}				
+		return s;
+	}
+	
+	private static boolean createWP2xml(File annotationFile, File newFile, GoldStandardEdgesLoader gsloader){
+		try {
+			String s = "";
+			BufferedReader reader = new BufferedReader(new FileReader(annotationFile));
+			String line = reader.readLine();
+			while(!line.contains("<edge ")){ // copy node lines, and stop as soon as reach edges
+				s+=line+"\n";							
+				line = reader.readLine();
+			}
+			reader. close();
+			// now add all edges from the graph
+			EntailmentGraphRaw r = gsloader.getRawGraph();
+			r.applyTransitiveClosure(false);
+			
+			// build a mapping from text to all its ids
+			Map<String,Set<String>> textToIdsMap = new HashMap<String, Set<String>>();
+			for (String id : gsloader.getNodeTextById().keySet()){
+				String text = gsloader.getNodeTextById().get(id);
+				Set<String> idsOfText = new HashSet<String>();
+				if (textToIdsMap.containsKey(text)) idsOfText = textToIdsMap.get(text);
+				idsOfText.add(id);
+				textToIdsMap.put(text, idsOfText);
+			}
+			
+			// add edges to the file
+			for (EntailmentRelation edge : r.edgeSet()){
+				s+=getWP2edge(edge, textToIdsMap);
+			}			
+			
+			s+="</F_entailment_graph>\n";
+			BufferedWriter writer = new BufferedWriter(new FileWriter(newFile));
+			writer.write(s);
+			writer.close();
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public static void createWP2Data(){
+		String gsAnnotationsDir = "D:/LiliGit/Excitement-Transduction-Layer/tl/src/test/resources/WP2_gold_standard_annotation/GRAPH-ENG-SPLIT-2014-03-24-FINAL/Test";
+		File gsDir = new File(gsAnnotationsDir);
+		for(String clusterAnnotationDir: gsDir.list()){
+			try {
+				File f = new File(gsAnnotationsDir+"/"+clusterAnnotationDir);
+				if (f.isDirectory()){
+					System.out.println(f.getName().toUpperCase());
+					GoldStandardEdgesLoader gsloader = new GoldStandardEdgesLoader();		
+					// load merge-graph annotations	
+					// clusterAnnotationDir should contain a folder called "FinalMergedGraph" with a single xml file with annotations
+					File clusterAnnotationMergedGraphDir = new File (gsAnnotationsDir+"/"+clusterAnnotationDir+"/"+"FinalMergedGraph");
+					System.out.println(clusterAnnotationMergedGraphDir.getAbsolutePath());
+					if (clusterAnnotationMergedGraphDir.isDirectory()){
+						System.out.println(f.getName().toUpperCase()+" LOADING");
+						for (File annotationFile : clusterAnnotationMergedGraphDir.listFiles()){
+							if (annotationFile.getName().endsWith(".xml")){
+								gsloader.addAnnotationsFromFile(annotationFile.getPath());
+								System.out.println(f.getName().toUpperCase()+" GS LOADED");
+								if (createWP2xml(annotationFile, new File(gsAnnotationsDir+"/"+clusterAnnotationDir+"/FinalMergedGraph/"+annotationFile.getName().replace(".xml", "PlusClosure.xml")), gsloader)){
+									System.out.println(f.getName().toUpperCase()+" CLOSURE XML IS CREATED");									
+								}
+								else System.out.println(f.getName().toUpperCase()+" CLOSURE XML COULD NOT BE CREATED");
+							}
+						}
+					}	
+				}
+			} catch (GraphEvaluatorException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//String gsAnnotationsDir = "D:/LiliGit/Excitement-Transduction-Layer/tl/src/test/resources/WP2_gold_standard_annotation/GRAPH-ITA-SPLIT-2014-03-14-FINAL/Test";
-		String gsAnnotationsDir = "D:/LiliGit/Excitement-Transduction-Layer/tl/src/test/resources/WP2_gold_standard_annotation/GRAPH-ENG-SPLIT-2014-03-24-FINAL/Test";
-		String resSting ="Cluster"+"\t"+"origRawNodes"+"\t"+"origRawEdges"+"\t"+"noClosureRawEdges"+"\t"+"withClosureRawEdges"+"\t"+"origCollapsedNodes"+"\t"+"origCollapsedEdges"+"\t"+"noClosureRawEdges"+"\t"+"withClosureRawEdges"+"\n";
-		File gsDir = new File(gsAnnotationsDir);
-		for(String s: gsDir.list()){
-			File f = new File(gsAnnotationsDir+"/"+s);
-			if (f.isDirectory()){
-				System.out.println(f.getName().toUpperCase());
-				try {
-					GoldStandardEdgesLoader gsloader = new GoldStandardEdgesLoader();
-					gsloader.loadClusterAnnotations(f.getAbsolutePath(), false);
-					EntailmentGraphRaw r = gsloader.getRawGraph();
-					System.out.print("Raw graph.\nNumber of edges: ");
-					int origRawEdges = r.edgeSet().size();
-					System.out.println(origRawEdges);
-					System.out.print("Number of nodes: ");
-					int origRawNodes = r.vertexSet().size();
-					System.out.println(origRawNodes);
-					r.toDOT("D:/Lili/NICE dadasets/_finalSplitLili/all.collapsed.row/"+f.getName().toUpperCase()+"_raw.dot.txt");
-					r.toXML("D:/Lili/NICE dadasets/_finalSplitLili/all.collapsed.row/"+f.getName().toUpperCase()+"_raw.xml");
-
-					
-					r.removeTransitiveClosure();
-					System.out.print("Raw graph -MinusClosure.\nNumber of edges: ");
-					int noClosureRawEdges = r.edgeSet().size();
-					System.out.println(noClosureRawEdges);
-					System.out.print("Number of nodes: ");
-					int noClosureRawNodes = r.vertexSet().size(); 
-					System.out.println(noClosureRawNodes);
-					r.toDOT("D:/Lili/NICE dadasets/_finalSplitLili/all.collapsed.row/"+f.getName().toUpperCase()+"_rawNoClosure.dot.txt");
-					r.toXML("D:/Lili/NICE dadasets/_finalSplitLili/all.collapsed.row/"+f.getName().toUpperCase()+"_rawNoClosure.xml");
-
-					
-					r.applyTransitiveClosure(false);
-					System.out.print("Raw graph+Closure.\nNumber of edges: ");
-					int withClosureRawEdges = r.edgeSet().size(); 
-					System.out.println(withClosureRawEdges);
-					System.out.print("Number of nodes: ");
-					int withClosureRawNodes =r.vertexSet().size(); 
-					System.out.println(withClosureRawNodes);
-					r.toDOT("D:/Lili/NICE dadasets/_finalSplitLili/all.collapsed.row/"+f.getName().toUpperCase()+"_rawClosure.dot.txt");
-					r.toXML("D:/Lili/NICE dadasets/_finalSplitLili/all.collapsed.row/"+f.getName().toUpperCase()+"_rawClosure.xml");
-
-					EntailmentGraphCollapsed c = gsloader.getCollapsedGraph();
-					System.out.print("Collapsed graph.\nNumber of edges: ");
-					int origCollapsedEdges = c.edgeSet().size(); 
-					System.out.println(origCollapsedEdges);
-					System.out.print("Number of nodes: ");
-					int origCollapsedNodes = c.vertexSet().size(); 
-					System.out.println(origCollapsedNodes);
-					c.toDOT("D:/Lili/NICE dadasets/_finalSplitLili/all.collapsed.row/"+f.getName().toUpperCase()+"_collapsed.dot.txt", gsloader.getNodeTextById());
-					c.toXML("D:/Lili/NICE dadasets/_finalSplitLili/all.collapsed.row/"+f.getName().toUpperCase()+"_collapsed.xml");
-					
-					c.removeTransitiveClosure();
-					System.out.print("Collapsed graph -MinusClosure.\nNumber of edges: ");
-					int noClosureCollapsedEdges = c.edgeSet().size(); 
-					System.out.println(noClosureCollapsedEdges);
-					System.out.print("Number of nodes: ");
-					int noClosureCollapsedNodes = c.vertexSet().size(); 
-					System.out.println(noClosureCollapsedNodes);
-					c.toDOT("D:/Lili/NICE dadasets/_finalSplitLili/all.collapsed.row/"+f.getName().toUpperCase()+"_collapsedNoClosure.dot.txt", gsloader.getNodeTextById());
-					c.toXML("D:/Lili/NICE dadasets/_finalSplitLili/all.collapsed.row/"+f.getName().toUpperCase()+"_collapsedNoClosure.xml");
-
-					
-					c.applyTransitiveClosure(true);
-					System.out.print("Collapsed graph + closure.\nNumber of edges: ");
-					int withClosureCollapsedEdges = c.edgeSet().size(); 
-					System.out.println(withClosureCollapsedEdges);
-					System.out.print("Number of nodes: ");
-					int withClosureCollapsedNodes = c.vertexSet().size();
-					System.out.println(withClosureCollapsedNodes);
-					c.toDOT("D:/Lili/NICE dadasets/_finalSplitLili/all.collapsed.row/"+f.getName().toUpperCase()+"_collapsedClosure.dot.txt", gsloader.getNodeTextById());
-					c.toXML("D:/Lili/NICE dadasets/_finalSplitLili/all.collapsed.row/"+f.getName().toUpperCase()+"_collapsedClosure.xml");
-					
-					resSting +=f.getName().toUpperCase()+"\t"+String.valueOf(origRawNodes)+"\t"+String.valueOf(origRawEdges)+"\t"+String.valueOf(noClosureRawEdges)+"\t"+String.valueOf(withClosureRawEdges)+"\t"+String.valueOf(origCollapsedNodes)+"\t"+String.valueOf(origCollapsedEdges)+"\t"+String.valueOf(noClosureCollapsedEdges)+"\t"+String.valueOf(withClosureCollapsedEdges)+"\n";
-	//				resSting +=f.getName().toUpperCase()+"\t"+String.valueOf(origRawNodes)+"\t"+String.valueOf(origRawEdges)+"\t"+"---"+"\t"+"---"+"\t"+String.valueOf(origCollapsedNodes)+"\t"+String.valueOf(origCollapsedEdges)+"\t"+String.valueOf(noClosureCollapsedEdges)+"\t"+String.valueOf(withClosureCollapsedEdges)+"\n";
-				} catch (GraphEvaluatorException | GraphOptimizerException | IOException | EntailmentGraphCollapsedException | TransformerException | EntailmentGraphRawException e) {
-
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-									
-				}							
-			}
-		}
-		System.out.println(resSting);
+		
+		// GoldStandardAnalyzer.getStatistics();
+		createWP2Data();
+		
 	}
 	
 	
-
+		
 }
