cd2f1d56a502f48b0189fefa76f819bd73c765c6
LiliKotlerman <lili.dav@gmail.com>
2014-03-25 16:07:24 +0200

    minor

--- tl/src/main/java/eu/excitementproject/tl/experiments/NICE/SemevalStuff.java
+++ tl/src/main/java/eu/excitementproject/tl/experiments/NICE/SemevalStuff.java
@@ -19,8 +19,8 @@ public class SemevalStuff {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String gsAnnotationsDir = "D:/LiliGit/Excitement-Transduction-Layer/tl/src/test/resources/WP2_gold_standard_annotation/GRAPH-ITA-SPLIT-2014-03-14-FINAL/Test";
-//		String gsAnnotationsDir = "D:/LiliGit/Excitement-Transduction-Layer/tl/src/test/resources/WP2_gold_standard_annotation/GRAPH-ENG-SPLIT-2014-03-24-FINAL/Test";
+		//String gsAnnotationsDir = "D:/LiliGit/Excitement-Transduction-Layer/tl/src/test/resources/WP2_gold_standard_annotation/GRAPH-ITA-SPLIT-2014-03-14-FINAL/Test";
+		String gsAnnotationsDir = "D:/LiliGit/Excitement-Transduction-Layer/tl/src/test/resources/WP2_gold_standard_annotation/GRAPH-ENG-SPLIT-2014-03-24-FINAL/Test";
 		String resSting ="Cluster"+"\t"+"origRawNodes"+"\t"+"origRawEdges"+"\t"+"noClosureRawEdges"+"\t"+"withClosureRawEdges"+"\t"+"origCollapsedNodes"+"\t"+"origCollapsedEdges"+"\t"+"noClosureRawEdges"+"\t"+"withClosureRawEdges"+"\n";
 		File gsDir = new File(gsAnnotationsDir);
 		for(String s: gsDir.list()){
