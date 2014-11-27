a640863a22ffbbde9403dbf0e17a949c9037a3ea
LiliKotlerman <lili.dav@gmail.com>
2014-11-27 12:07:39 +0200

    cleanup more exceptions

--- tl/src/main/java/eu/excitementproject/tl/decomposition/exceptions/FragmentGraphGeneratorException.java
+++ tl/src/main/java/eu/excitementproject/tl/decomposition/exceptions/FragmentGraphGeneratorException.java
@@ -3,22 +3,23 @@
  */
 package  eu.excitementproject.tl.decomposition.exceptions;
 
-/** Exception thrown by FragmentGraphGenerator if any of the needed data is missing in the input CAS or if the implementation can't generate the graphs for some
-  reason
- * @author Lili
+import eu.excitementproject.tl.decomposition.api.FragmentGraphGenerator;
+
+/** Exception thrown by {@link FragmentGraphGenerator} if any of the needed data is missing 
+ * in the input CAS or if the implementation cannot generate the graphs for some  reason
+ * @author Lili Kotlerman
  *
  */
 public class FragmentGraphGeneratorException extends Exception {
 	
 	private static final long serialVersionUID = 2565175967769513901L;
 
-	/** Exception thrown by FragmentGraphGenerator if any of the needed data is missing in the input CAS or if the implementation can't generate the graphs for some
-  reason
+	/** Exception thrown by {@link FragmentGraphGenerator} if any of the needed data is missing 
+	 * in the input CAS or if the implementation cannot generate the graphs for some  reason
 	 * @param message
 	 */
 	public FragmentGraphGeneratorException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	
--- tl/src/main/java/eu/excitementproject/tl/decomposition/exceptions/ModifierAnnotatorException.java
+++ tl/src/main/java/eu/excitementproject/tl/decomposition/exceptions/ModifierAnnotatorException.java
@@ -3,8 +3,11 @@
  */
 package  eu.excitementproject.tl.decomposition.exceptions;
 
+import eu.excitementproject.tl.decomposition.api.ModifierAnnotator;
 
-/** Exception thrown by ModifierAnnotator if any of the needed data is missing in the input CAS or if couldn't annotate the modifiers
+
+/** Exception thrown by {@link ModifierAnnotator} if any of the needed data is missing 
+ * in the input CAS or if it could not annotate the modifiers
  * @author Lili
  *
  */
@@ -15,13 +18,19 @@ public class ModifierAnnotatorException extends Exception{
 	 */
 	private static final long serialVersionUID = 6791752728203555997L;
 
-	/** Exception thrown by ModifierAnnotator if any of the needed data is missing in the input CAS or if couldn't annotate the modifiers
+	/** Exception thrown by {@link ModifierAnnotator} if any of the needed data is missing 
+	 * in the input CAS or if it could not annotate the modifiers
 	 * @param message
 	 */
 	public ModifierAnnotatorException(String message) {
 		super(message);
 	}
 
+	/** Exception thrown by {@link ModifierAnnotator} if any of the needed data is missing 
+	 * in the input CAS or if it could not annotate the modifiers
+	 * @param message
+	 * @param cause
+	 */
 	public ModifierAnnotatorException(String message, Throwable cause) {
 		super(message, cause);
 	}
