48a7bddf6ba3b7d6410413b0f8c826817fa45db3
LiliKotlerman <lili.dav@gmail.com>
2014-11-27 10:24:21 +0200

    Clean javaDoc for GraphMerger intergface and AbstractGraphMerger

--- tl/src/main/java/eu/excitementproject/tl/composition/graphmerger/AbstractGraphMerger.java
+++ tl/src/main/java/eu/excitementproject/tl/composition/graphmerger/AbstractGraphMerger.java
@@ -10,7 +10,6 @@ import org.apache.log4j.Logger;
 
 import eu.excitementproject.eop.common.DecisionLabel;
 import eu.excitementproject.eop.common.EDABasic;
-import eu.excitementproject.eop.lap.LAPException;
 import eu.excitementproject.tl.composition.api.GraphMerger;
 import eu.excitementproject.tl.composition.exceptions.EntailmentGraphRawException;
 import eu.excitementproject.tl.composition.exceptions.GraphMergerException;
@@ -23,29 +22,48 @@ import eu.excitementproject.tl.structures.rawgraph.EntailmentUnit;
 
 /**
  * 
-An implementation of the {@link GraphMerger} interface:
-An implementation need to call LAP and EDA.
-The needed LAP & EDA related configurations should be passed via the
-Constructor (Thus, they are not defined in the interface). Also, any
-additional configurable parameters of this module implementation
-should be clearly exposed in the Constructor.
+Abstract implementation of the {@link GraphMerger} interface. 
+The class contains methods that are likely to be shared by the actual implementations, including
+constructors with LAP and EDA configurations.
 
  * @author Lili Kotlerman
  */
 public abstract class AbstractGraphMerger implements GraphMerger{
 
-	public int edaCalls = 0;
-	
 	Logger logger = Logger.getLogger("eu.excitementproject.tl.composition.graphmerger");
-	Double entailmentConfidenceThreshold = null;
 
+	/******************************************************************************************
+	 * ATTRIBUTES
+	 * ****************************************************************************************/
+	
+	/**
+	 * Holds the number of EDA calls performed by the merger. Is null if this information is not available.
+	 * Should be initialized as zero in a constructor and updated after each actual EDA call. 
+	 */
+	public Integer edaCalls = null;
+
+	/**
+	 * Holds the predefined threshold for confidence of entailment decisions. 
+	 * Is null when no threshold is defined. 
+	 * <p>Should be set by the method {@link GraphMerger#setEntailmentConfidenceThreshold(Double)}.
+	 */
+	public Double entailmentConfidenceThreshold = null;
+	
+	/**
+	 * The LAP to be used by the merger.
+	 */
 	private final CachedLAPAccess lap;
+	
+	/**
+	 * The EDA to be used by the merger.
+	 */
 	private final EDABasic<?> eda;
 	
-	/** The needed LAP & EDA related configurations should be passed via the
-Constructor (Thus, they are not defined in the interface). Also, any
-additional configurable parameters of this module implementation
-should be clearly exposed in the Constructor.
+	/******************************************************************************************
+	 * CONSTRUCTORS
+	 * ****************************************************************************************/
+
+	/** Constructor, which specifies the LAP and the EDA to be used by the merger.
 	 * @param lap
 	 * @param eda
 	 * @throws GraphMergerException
@@ -56,6 +74,10 @@ should be clearly exposed in the Constructor.
 		edaCalls=0;
 	}
 
+	/******************************************************************************************
+	 * IMPLEMENTATION OF THE INTERFACE METHODS
+	 * ****************************************************************************************/
+
 	@Override
 	public EntailmentGraphRaw mergeGraphs(Set<FragmentGraph> fragmentGraphs)
 			throws GraphMergerException {
@@ -63,28 +85,51 @@ should be clearly exposed in the Constructor.
 		return mergeGraphs(fragmentGraphs, null);
 	}	
 	
+	@Override
+	public Integer getEdaCallsNumber() {
+		return edaCalls;
+	}
+
+	@Override
+	public void setEntailmentConfidenceThreshold(Double entailmentConfidenceThreshold) {
+		this.entailmentConfidenceThreshold = entailmentConfidenceThreshold;
+	}
+
+	/******************************************************************************************
+	 * ACCESS TO ATTRIBUTES
+	 * ****************************************************************************************/
+
 	/**
-	 * @return the LAP passed in the constructor to the GraphMerger implementation
+	 * @return the LAP defined for this merger (passed in the constructor)
 	 */
 	public CachedLAPAccess getLap() {
 		return this.lap;
 	}
 	
 	/**
-	 * @return the EDA passed in the constructor to the GraphMerger implementation
+	 * @return the EDA defined for this merger (passed in the constructor)
 	 */
 	public EDABasic<?> getEda() {
 		return this.eda;
 	}
 
+	/**
+	 * @return the entailmentConfidenceThreshold
+	 */
+	public Double getEntailmentConfidenceThreshold() {
+		return entailmentConfidenceThreshold;
+	}
 	
-	/** Checks for entailment (in both directions) between nodeA and nodeB. If there is entailment in any direction, corresponding EntailmentRelation(s) will be returned.
-	 * The yes/no decision on entailment takes into considerartion the value of entailmentConfideneThreshold, if defined.   
-	 * @param workGraph
+	/******************************************************************************************
+	 * AUXILIARY METHODS FOR MERGING
+	 * ****************************************************************************************/
+
+	/** Make EDA calls to check for entailment (in both directions) between nodeA and nodeB. If there is entailment in any direction, corresponding EntailmentRelation(s) ({@link EntailmentRelation}) will be returned.
+	 * <p>The yes/no decision on entailment takes into considerartion the value of {@link AbstractGraphMerger#entailmentConfidenceThreshold}, if defined.   
 	 * @param nodeA
 	 * @param nodeB
 	 * @return set of entailment relations found between the nodes. The set can contain 0 (no entailment), 1 (entailment in one direction) or 2 (bi-directional entailment, paraphrase) elements.
-	 * @throws LAPException 
+	 * @throws GraphMergerException
 	 */
 	protected Set<EntailmentRelation> getEntailmentRelations(EntailmentUnit nodeA, EntailmentUnit nodeB) throws GraphMergerException{
 		Set<EntailmentRelation> entailmentRelations = new HashSet<EntailmentRelation>();
@@ -102,7 +147,32 @@ should be clearly exposed in the Constructor.
 		return entailmentRelations;
 	}
 
+	/** Make an EDA call and return the corresponding {@link EntailmentRelation} if there is entailment candidateEntailingNode -> candidateEntailedNode.
+	 * <p> Decision whether there is entailment is based on the {@link DecisionLabel} returned by the EDA, and takes into consideration the value of {@link AbstractGraphMerger#entailmentConfidenceThreshold}.
+	 * 
+	 * @param candidateEntailingNode
+	 * @param candidateEntailedNode
+	 * 
+	 * @return Return the corresponding {@link EntailmentRelation} if the conditions are fulfilled. Return null otherwise.
+	 * @throws GraphMergerException 
+	 */
+	protected EntailmentRelation getEntailmentRelation(EntailmentUnit candidateEntailingNode, EntailmentUnit candidateEntailedNode) throws GraphMergerException{	
+		// check only one direction: candidateEntailingNode -> candidateEntailedNode
+		EntailmentRelation r = getRelation(candidateEntailingNode, candidateEntailedNode);
+		logger.info("\t'"+candidateEntailingNode.getTextWithoutDoubleSpaces() +"'\t->\t'"+candidateEntailedNode.getTextWithoutDoubleSpaces()+"'\t"+r.getLabel().toString());
+		if (r.getLabel().is(DecisionLabel.Entailment)) {
+			if (isSufficientConfidence(r.getConfidence()))  return r;			
+		}
+		return null;
+	}
 
+	/** Make EDA calls and return the resulting {@link EntailmentRelation}s (in both directions) between nodeA and nodeB.
+	 * <p> Does not take into consideration the value of {@link AbstractGraphMerger#entailmentConfidenceThreshold}.
+	 * @param nodeA
+	 * @param nodeB
+	 * @return set of {@link EntailmentRelation}s between the two nodes. The set should contains 2 elements, for the two entailment directions.
+	 * @throws GraphMergerException
+	 */
 	protected Set<EntailmentRelation> getRelations(EntailmentUnit nodeA, EntailmentUnit nodeB) throws GraphMergerException{
 		Set<EntailmentRelation> entailmentRelations = new HashSet<EntailmentRelation>();
 		
@@ -115,9 +185,17 @@ should be clearly exposed in the Constructor.
 	}
 
 	
-	
-	 // viv@fbk: added lap parameter
-	 			
+		 			
+	/** Make an EDA call and return the resulting {@link EntailmentRelation} for candidateEntailingNode -> candidateEntailedNode.
+	 * <p> Does not take into consideration the value of {@link AbstractGraphMerger#entailmentConfidenceThreshold}.
+	 * <p> The method performs the actual EDA call, and thus updates the value of {@link AbstractGraphMerger#edaCalls}. 
+
+	 * @param candidateEntailingNode
+	 * @param candidateEntailedNode
+	 * 
+	 * @return Return the corresponding {@link EntailmentRelation}.
+	 * @throws GraphMergerException 
+	 */
 	protected EntailmentRelation getRelation(EntailmentUnit candidateEntailingNode, EntailmentUnit candidateEntailedNode) throws GraphMergerException{	
 		edaCalls++;
 		// check only one direction: candidateEntailingNode -> candidateEntailedNode
@@ -128,46 +206,15 @@ should be clearly exposed in the Constructor.
 		}
 	}
 
-	public int getEdaCallsNumber() {
-		return edaCalls;
-	}
-
+	/** Check if the given confidence is sufficient to consider an entailment decision valid, based on the predefined threshold {@link AbstractGraphMerger#entailmentConfidenceThreshold}.
+	 * @param confidence - the given confidence to check
+	 * @return true if the given confidence is >= the threshold, otherwise return false. If no threshold was defined, return true.
+	 */
 	private boolean isSufficientConfidence(double confidence){
 		if (entailmentConfidenceThreshold==null) return true; //if entailmentConfidenceThreshold is not defined, then any confidence is sufficient to add an edge
 		if (confidence >= entailmentConfidenceThreshold) return true; // if confidence >= threshold, then it is sufficient 
 		return false; // otherwise - it's not sufficient
 	}
 	
-	/** Return the corresponding EntailmentRelation if there is entailment (DecisionLabel.Entailment) candidateEntailingNode -> candidateEntailedNode
-	 * and the confidence is above entailmentConfidenceThreshold (if provided)
-	 * Return null otherwise 
-	 * @param candidateEntailingNode
-	 * @param candidateEntailedNode
-	 * @return
-	 * @throws LAPException 
-	 */
-	protected EntailmentRelation getEntailmentRelation(EntailmentUnit candidateEntailingNode, EntailmentUnit candidateEntailedNode) throws GraphMergerException{	
-		// check only one direction: candidateEntailingNode -> candidateEntailedNode
-		EntailmentRelation r = getRelation(candidateEntailingNode, candidateEntailedNode);
-		logger.info("\t'"+candidateEntailingNode.getTextWithoutDoubleSpaces() +"'\t->\t'"+candidateEntailedNode.getTextWithoutDoubleSpaces()+"'\t"+r.getLabel().toString());
-		if (r.getLabel().is(DecisionLabel.Entailment)) {
-			if (isSufficientConfidence(r.getConfidence()))  return r;			
-		}
-		return null;
-	}
 
-	/**
-	 * @return the entailmentConfidenceThreshold
-	 */
-	public Double getEntailmentConfidenceThreshold() {
-		return entailmentConfidenceThreshold;
-	}
-
-	/**
-	 * @param entailmentConfidenceThreshold the entailmentConfidenceThreshold to set
-	 */
-	@Override
-	public void setEntailmentConfidenceThreshold(Double entailmentConfidenceThreshold) {
-		this.entailmentConfidenceThreshold = entailmentConfidenceThreshold;
-	}
 }
