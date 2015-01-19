700143467828dcae3041ceb9a9aed8ba75ac0f5d
salvadora <aleksgab@gmail.com>
2015-01-19 19:23:43 +0100

    added some prints for debugging

--- tl/src/main/java/eu/excitementproject/tl/evaluation/categoryannotator/EvaluatorUtils.java
+++ tl/src/main/java/eu/excitementproject/tl/evaluation/categoryannotator/EvaluatorUtils.java
@@ -124,6 +124,7 @@ public class EvaluatorUtils {
 		for (int i=0; i<topN; i++) { //adapted to consider top N categories
 			String cat = bestCats[i];
 			logger.info("Top " + i + " category: " + cat);
+			System.out.print("Top " + i + " category:\t" + cat + "\t");
 			if (docCats.contains(cat)) { //adapted to deal with multiple categories
 				countPositive++;
 			} 
@@ -401,6 +402,7 @@ public class EvaluatorUtils {
 
 		Map<String,BigDecimal> sortedMapBigDecimal = new TreeMap<String,BigDecimal>(bvc);
 		sortedMapBigDecimal.putAll(categoryScoresBigDecimal);		
+		System.out.print("best cat score: " + sortedMapBigDecimal + "\t");
 		
 		logger.info("category scores:  " + sortedMapBigDecimal);
 		String[] bestCats = new String[topN];
